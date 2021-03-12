9d71318a2250e3366b21b40491736d6066f6218e
github-@Маргарита#6359 <rita.maletz@gmail.com>
2021-03-12 21:48:58 +0300

    feat: add solution for transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,36 @@
-const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  if (!Array.isArray(arr)) {
+    throw Error();
+  }
+  
+  let resArr = new Array();
+  for (let i=0; i<arr.length; i++){
+    switch (arr[i]) {
+      case '--discard-next': 
+        i++;
+        break;
+      case '--discard-prev': 
+        if (!(i===0)) {
+          if (arr[i-2] !== '--discard-next' && arr[i-1] !== undefined) {
+          resArr.pop();
+          }
+        }
+         break;
+      case '--double-next': 
+        if (arr[i+1] !== undefined) resArr.push(arr[i+1]);
+        break;
+      case '--double-prev': 
+        if (!(i===0)) {
+          if (arr[i-2] !== '--discard-next' && arr[i-1] !== undefined) {
+            resArr.push(arr[i-1]); 
+          }  
+        }
+        break; 
+      default: 
+        resArr.push(arr[i]);
+        break;
+    }
+  }
+  return resArr;
 };
