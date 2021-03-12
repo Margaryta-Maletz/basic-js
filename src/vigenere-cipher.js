e88d347fcfef3248704a600a3b7bbe571a34f732
github-@Маргарита#6359 <rita.maletz@gmail.com>
2021-03-12 16:27:43 +0300

    feat: add solution for vigenere-cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,58 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  
+  constructor(flag) {
+    this.flag = flag;
+  }
+
+  encrypt(str, key) {
+    if (str === 'undefined' || key === 'undefined') {
+      return Error();
+    }
+    const strKey = key.toUpperCase().repeat(Math.ceil(str.length/key.length));
+    let counter = 0,
+        index, 
+        result ='';
+    str = str.toUpperCase();
+    for (let i = 0; i < str.length; i++){
+      index = 65+(strKey.charCodeAt(counter)-65+str.charCodeAt(i)-65)%26;
+      if (str.charCodeAt(i) >= 65 && str.charCodeAt(i) <= 90) {
+        result += String.fromCharCode(index);
+        counter++;
+      } else {
+        result += str[i];
+      }
+    }
+    if (this.flag === false) {
+      return result.split('').reverse().join('');
+    }  
+    return result;
   }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+
+  
+  decrypt(str, key) {
+    if (str === 'undefined' || key === 'undefined') {
+      return Error();
+    }
+    const strKey = key.toUpperCase().repeat(Math.ceil(str.length/key.length));
+    let counter = 0,
+        index, 
+        result ='';
+    str = str.toUpperCase();
+    for (let i = 0; i < str.length; i++){
+      index = 65+(26+str.charCodeAt(i)-strKey.charCodeAt(counter))%26;
+      if (str.charCodeAt(i) >= 65 && str.charCodeAt(i) <= 90) {
+        result += String.fromCharCode(index);
+        counter++;
+      } else {
+        result += str[i];
+      }
+    }
+    if (this.flag === false) {
+      return result.split('').reverse().join('');
+    }  
+    return result;
   }
 }
 
