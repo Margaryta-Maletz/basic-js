dfe770dc0c80f49c13110fc96dfc5f78e588ce06
github-@Маргарита#6359 <rita.maletz@gmail.com>
2021-03-12 18:00:58 +0300

    feat: add solution for extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,28 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+  let arr = [];
+  if (options.additionRepeatTimes === undefined && !(options.addition === undefined)) {
+    arr.push(options.addition);
+  }
+
+  for (let i = 0; i < options.additionRepeatTimes; i++) {
+    arr.push(String(options.addition));
+  }
+  (options.additionSeparator === undefined) ? str += arr.join('|') : str += arr.join(options.additionSeparator);
+
+  arr = [];
+  if (options.repeatTimes === undefined) {
+    arr.push(str);
+  }
+  for (let j = 0; j < options.repeatTimes; j++) {
+    arr.push(String(str));
+  }
+  (options.separator === undefined) ? str = arr.join('+') : str = arr.join(options.separator);
+
+  return str;
 };
   
+
+
+/*repeatTimes: 4, separator: '1L467Kdqx2', addition: 'IMqCarClDg', additionRepeatTimes: 8, additionSeparator: 'U7L9D0f8pb'*/
